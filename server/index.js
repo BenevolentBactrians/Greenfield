ac7ce99421f530f856ed2f512d9a1d8d8e64548e
Michael Flis <michaelflis07@gmail.com>
2018-04-12 14:55:10 -0700

    Add server-db routes to saveUser, saveTask and saveNote

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const db = require('../database');
 
 const app = express();
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('../client/dist'))
+app.use(morgan('dev')); // HTTP request logger middleware for node.js
+
+app.use(express.static('../client/dist'));
 
 app.get('/', (req, res) => {
-  res.redirect(301, '/login')
+  res.redirect(301, '/login');
 })
 
 app.get('/login', (req, res) => {
@@ -18,9 +22,27 @@ app.get('/login', (req, res) => {
 })
 
 app.post('/login', urlencodedParser, (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   res.sendStatus(201)
 })
 
+app.post('/saveUser', urlencodedParser, (req, res) => {
+  console.log(req.body);
+  db.saveUser(req.body);
+  res.sendStatus(201);
+})
 
-app.listen(process.env.PORT || 3000, () => console.log('listening on 3000'))
+app.post('/saveTask', urlencodedParser, (req, res) => {
+  console.log(req.body);
+  db.saveTask(req.body);
+  res.sendStatus(201);
+})
+
+app.post('/saveNote', urlencodedParser, (req, res) => {
+  console.log(req.body);
+  db.saveNote(req.body);
+  res.sendStatus(201);
+})
+
+
+app.listen(process.env.PORT || 3000, () => console.log('listening on 3000')); // TODO update this console log
