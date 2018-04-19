f646d3b3a522dd4c3cb5a6100eac22ae0e301c19
vasyl-n <vasyl.nrk@gmail.com>
2018-04-19 00:13:56 -0700

    squash commit

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,46 @@
-App.jsx
-Login.jsx
-Weekly.jsx
-Register.jsx
-AddTask.jsx
-Notes.jsx
-Daily.jsx
-AppHeader.jsx
-DailyHeader.jsx
-TaskTimeRange.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import MuiThemeProvider from 'material-ui;
+// import SvgIcon from 'material-ui/SvgIcon';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(props) {
+    return (
+      // <MuiThemeProvider>
+
+        <div className="container">
+
+
+          <div className="nav-left"> LEFT  </div>
+          <div className="nav-center"> <h1> TITLE </h1> </div>
+          <div className="nav-right"> RIGHT </div>   
+
+            
+          <div className='col-left'>
+            <div className='add-task'> ADD TASK </div>
+            <div className='duck-view'> DUCK </div>
+          </div>
+          
+          <div className='col-center'>
+            <div className='weekly-view'> WEEKLY OUTLOOK </div>
+          </div>
+          
+          <div className='col-right'>
+            <div className='notes'>NOTES</div>
+            <div className='chart'>CHART</div>
+          </div>
+
+
+        </div>
+       
+       // </MuiThemeProvider>
+
+      )
+  }
+}
+
+export default App
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,43 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-
-          <div className="container">
-
-            <div className="header">  
-              <div className="header-left"> LEFT </div>
-              <div className="header-center"> <h1>TITLE </h1></div>
-              <div className="header-right"> RIGHT </div>   
-            </div>
-              
-            <div className='col-left'>
-              <div className='add-task'> ADD TASK </div>
-              <div className='duck-view'> ASK THE DUCK </div>
-            </div>
-            
-            <div className='col-center'>
-              <div className='weekly-view'> WEEKLY OUTLOOK </div>
-            </div>
-            
-            <div className='col-right'>
-              <div className='notes'>NOTES</div>
-              <div className='chart'>PROGRESS</div>
-            </div>
+// import MuiThemeProvider from 'material-ui;
+// import SvgIcon from 'material-ui/SvgIcon';
+import App from './components/App.jsx'
 
 
-          </div>
-
-
-      )
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
