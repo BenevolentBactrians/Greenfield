b2e0231e5ec30c2a0bc9c5006e322b0dce9706cd
Michael Flis <michaelflis07@gmail.com>
2018-04-19 23:03:59 -0700

    Complete Weekly UI mockup, add visual layout only

--- client/src/components/Weekly.jsx
+++ client/src/components/Weekly.jsx
@@ -3,10 +3,44 @@ import ReactDOM from 'react-dom';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
+import moment from 'moment';
+import RaisedButton from 'material-ui/RaisedButton';
 
-const style = {
+// test
+import ActionHome from 'material-ui/svg-icons/action/home';
+import SvgIcon from 'material-ui/SvgIcon';
+
+const iconStyles = {
+  margin: '8px 16px 8px 16px',
+};
+
+const LeftArrow = (props) => (
+  <SvgIcon {...props}> 
+    <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
+      <path d="M0-.5h24v24H0z" fill="none"/>
+    </svg>   
+  </SvgIcon>
+)
+
+const RightArrow = (props) => (
+  <SvgIcon {...props}> 
+    <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
+        <path d="M0-.25h24v24H0z" fill="none"/>
+    </svg>  
+  </SvgIcon>
+)
+
+
+const styleList = {
   display: 'inline-block',
-  margin: '16px 32px 16px 0',
+  margin: '16px 16px 8px 16px',
+};
+
+const styleBtn = {
+  margin: 16
 };
 
 
@@ -30,65 +64,48 @@ class Weekly extends React.Component {
     return (
      
       
-        <div>
-          <Paper style={style}>
-            <Menu>
-              <MenuItem primaryText="Maps" />
-              <MenuItem primaryText="Books" />
-              <MenuItem primaryText="Flights" />
-              <MenuItem primaryText="Apps" />
-            </Menu>
-          </Paper>
-          <Paper style={style}>
-            <Menu>
-              <MenuItem primaryText="Refresh" />
-              <MenuItem primaryText="Help &amp; feedback" />
-              <MenuItem primaryText="Settings" />
-              <MenuItem primaryText="Sign out" />
-            </Menu>
-          </Paper>
-        </div>
-         
+        <div className="week-view-container">
+          <Paper style={styleList}>
+            
+            <div className="week-title"> <h3>WEEKLY TASKS</h3></div>
+           
+            <div className="week-list">       
+              
+              <Paper style={styleList}>
+                <Menu>               
+                  { this.state.week.map( (day, index) =>
+                    <MenuItem key={index} primaryText={day.name} />                 
+                    )
+                  }               
+                </Menu>
+              </Paper>
+            
+            
+   
+              <Paper style={styleList}>
+                <Menu>  
+                  { this.state.week.map( (day, index) => 
+                    <MenuItem key={index} primaryText={`${day.total} Tasks`} />
+                    )             
+                  }
+                  
+                </Menu>
+              </Paper>
+ 
+          </div>
+          
+          <div className="week-selectors">            
+            <RaisedButton icon={<LeftArrow style={iconStyles} />} style={styleBtn} />
+            <RaisedButton label={<RightArrow style={iconStyles} />} style={styleBtn} />       
+          </div>
+          
+          
+        </Paper>
+       </div>  
       
     )
   }
   
 }
 
-export default Weekly
-
-     // <div className="week-view-container"> 
-        
-     //    <div className="week-view-title"> Weekly Tasks </div>
-        
-     //    <div className="week-list">
-          
-          
-     //      <div className="week-list-items">
-            
-     //        <ul>
-     //        { this.state.week.map( (day, index) =>
-              
-     //            <li key={index}>{day.name}</li>
-              
-     //          )
-     //         }
-     //        </ul>
-            
-     //      </div>
-          
-     //      <div className="week-list-sum">
-     //        <ul>
-     //          { this.state.week.map( (day, index) =>
-               
-     //              <li key={index}>{day.total}</li>
-                
-     //            )
-     //           }
-     //          </ul>
-     //      </div>
-          
-          
-     //    </div>
-      
-     //  </div>  
+export default Weekly;
