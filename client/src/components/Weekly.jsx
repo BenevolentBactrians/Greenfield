8873e7c1243c820d9b72c1fb3dc65dc55907f6fc
Michael Flis <michaelflis07@gmail.com>
2018-04-23 18:13:06 -0400

    Create weekly task entry component

--- client/src/components/Weekly.jsx
+++ client/src/components/Weekly.jsx
@@ -58,7 +58,7 @@ class Weekly extends React.Component {
             <div className="week-list">       
               
               <Paper style={listStyles}>
-                <Menu width={300}>               
+                <Menu width={320}>               
                   { this.props.week.map( (day, index) =>
                     <WeeklyTaskEntry day={day} key={index} />                 
                     )
--- client/src/components/WeeklyTaskEntry.jsx
+++ client/src/components/WeeklyTaskEntry.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import MenuItem from 'material-ui/MenuItem';
-import axios from 'axios';
-import moment from 'moment';
+// import axios from 'axios';
+// import moment from 'moment';
 
-const WeeklyTaskEntry = (props) => (
+class WeeklyTaskEntry extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {}
+  }
   
-  <MenuItem  
-    primaryText={props.day.name} 
-    secondaryText={`${props.day.total} Task(s)`} 
-    />
-)
+  render (props) {
+  
+  return ( 
+    <MenuItem  
+      primaryText={this.props.day.name} 
+      secondaryText={`${this.props.day.total} Task(s)`} 
+      onClick={(e)=> console.log('clicked ', this.props.day)}
+      />
+  )
+}
+  
+}
 
 export default WeeklyTaskEntry;
