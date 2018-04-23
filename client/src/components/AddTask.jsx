72dd143dc9c70e7db24133579539ffa85443a816
Eric Chi <echi8112@gmail.com>
2018-04-23 14:00:41 -0400

    restricts dates for DatePicker

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -119,7 +119,10 @@ class AddTask extends React.Component {
             <DatePicker
               id="date"
               minDate={this.state.today}
+<<<<<<< HEAD
               value={this.state.date}
+=======
+>>>>>>> restricts dates for DatePicker
               hintText="yyyy/mm/dd"
               onChange={this.onChangeDate}
               style={{
