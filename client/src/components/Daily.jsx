d8a824d88128f128a7a00d2e01df9b9b7046bfa8
Eric Chi <echi8112@gmail.com>
2018-04-26 16:28:08 -0400

    creates a Daily component which opens as a dialog from the WeeklyTaskEntry component

--- client/src/components/Daily.jsx
+++ client/src/components/Daily.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import DailyTaskEntry from './DailyTaskEntry.jsx';
 
+class Daily extends React.Component {
+  constructor(props) {
+    super(props)
 
-const Daily = (props) => (
+    this.state={
+      tasks:[]
+    }
 
-  <div> Daily </div>
-)
+    this.handleDelete = this.handleDelete.bind(this)
+  }
 
-export default Daily
+  componentDidMount() {
+    this.setState({
+      tasks: this.props.tasks
+    })
+  }
+
+  handleDelete(taskId) {
+    axios.delete(`http://localhost:3000/task/${taskId}`)
+    .then((res) => {
+        let tasks = this.state.tasks.slice()
+        this.setState({tasks: tasks.filter((task, ind) => task._id !== taskId) });
+      })
+
+  }
+
+  render() {
+    return (
+        <div className='daily-container'>
+          <div className='daily-header'>header</div>
+          <div className='daily-body'>
+            {this.props.tasks.map((task, index) => {
+              return (
+                <DailyTaskEntry task={task} handleDelete={this.handleDelete} key={index}/>
+                )
+            })}
+          </div>
+        </div>
+
+      )
+  }
+}
+
+export default Daily;
