bb5fced92b2a62c89b77901c6a653e8a52d4913b
Vasyl Nesterenko <24661400+vasyl-n@users.noreply.github.com>
2018-04-28 09:36:47 -0700

    Data for chart (#47)
    * supplies data to chart
    
    * some progress
    
    * chart data
    
    * database change for completed tasks

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,21 +1,42 @@
 import {Doughnut} from 'react-chartjs-2';
 import React from 'react';
 
-const Chart = (props) => {
-  const data = {
-    datasets: [{
-        data: [props.completed, props.todo],
-        backgroundColor: [
-          'rgba(0,255,0, 0.5)',
-          'rgba(255, 0, 0, 0.5)'
-        ]
-    }],
-    labels: ['Completed', 'Todo']
-};
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: 1,
+      todo: 2
+    }
+  }
 
-  return  (
-    <Doughnut data={data} />
-  )
+  static getDerivedStateFromProps = (nextProps, prevState) => {
+    let completed = 0;
+    let todo = 0;
+    nextProps.data.forEach((el, ind) => {
+      if(el.tasks.length) {
+        el.tasks.forEach((task, index) => {
+          task.completed ? completed = completed + 1 : todo = todo + 1;
+        })
+      }
+    })
+    return {completed: completed, todo: todo}
+  }
+
+  render() {
+    return  (
+      <Doughnut data={{
+        datasets: [{
+            data: [this.state.completed, this.state.todo],
+            backgroundColor: [
+              'rgba(0,255,0, 0.5)',
+              'rgba(255, 0, 0, 0.5)'
+            ]
+        }],
+        labels: ['Completed', 'Todo']
+      }} />
+    )
+  }
 }
 
 Chart.defaultProps = {
