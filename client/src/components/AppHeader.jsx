cfc7e9b6b1f7a446f2eaec38eeb8d135e44a4935
Phillip Schmidt <markidex@gmail.com>
2018-05-07 16:39:41 -0700

    Moved New Task Popup over to right, SideBar look is cleaner

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -43,22 +43,23 @@ class Logged extends React.Component {
   }
   render() {
     return (
-      <IconMenu
-        iconButtonElement={
-          <IconButton> 
-            <svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0h24v24H0z" fill="none"/>
-              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
-            </svg>
-          </IconButton>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <Link to="/" style={{ textDecoration: 'none' }}><MenuItem primaryText="Sign out" onClick={this.handleSignOut} ></MenuItem></Link>
-      </IconMenu>
+      <div></div>
+      // <IconMenu
+      //   iconButtonElement={
+      //     <IconButton> 
+      //       <svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+      //         <path d="M0 0h24v24H0z" fill="none"/>
+      //         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+      //       </svg>
+      //     </IconButton>
+      //   }
+      //   targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      //   anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      // >
+      //   <MenuItem primaryText="Refresh" />
+      //   <MenuItem primaryText="Help" />
+      //   <Link to="/" style={{ textDecoration: 'none' }}><MenuItem primaryText="Sign out" onClick={this.handleSignOut} ></MenuItem></Link>
+      // </IconMenu>
     )
   }
 };
@@ -92,8 +93,14 @@ class AppHeader extends React.Component {
           </div>
           <Route exact path="/signup" render={()=><Register setUserIdToState={this.props.setUserIdToState} isActive={true}/>}/>
           <Route exact path="/login" render={()=><LoginView setUserIdToState={this.props.setUserIdToState} isActive={true}/>}/>
+
+          <button type="button" className="btn btn-info tsk-btn"> Add Task </button>
+
           
           <Duck className='duck-view' />
+
+          <button type="button" className="btn btn-info app-btn"> Sign Out </button>
+          
         </div>
       </Ddiv>
     )
